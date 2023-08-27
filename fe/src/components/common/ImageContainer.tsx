41f5559b6ae4b8d2c98deb3e76a30fa32440837e
JH <vvaa277@gmail.com>
2023-08-28 01:32:07 +0900

    feat: 이미지 컨테이너 컴포넌트 구현

---
+++ fe/src/components/common/ImageContainer.tsx
@@ -0,0 +1,50 @@
+import { Theme, css } from '@emotion/react';
+
+type Props = {
+  variant?: 'rectangle' | 'circle';
+  size: 's' | 'm' | 'l';
+  imgUrl: string;
+};
+
+export const ImageContainer: React.FC<Props> = ({
+  variant = 'rectangle',
+  size,
+  imgUrl,
+}) => {
+  return (
+    <div css={(theme) => ImageContainerStyle(theme, variant, size, imgUrl)}>
+      {/* <img src={imgUrl} alt="" /> */}
+    </div>
+  );
+};
+
+const ImageContainerStyle = (
+  theme: Theme,
+  variant: 'rectangle' | 'circle',
+  size: 's' | 'm' | 'l',
+  imgUrl: string,
+) => {
+  const SIZE_STYLES = {
+    s: {
+      width: '48px',
+      height: '48px',
+      borderRadius: variant === 'circle' ? '50%' : '8px',
+    },
+    m: {
+      width: '80px',
+      height: '80px',
+      borderRadius: '16px',
+    },
+    l: {
+      width: '120px',
+      height: '120px',
+      borderRadius: '8px',
+    },
+  };
+
+  return css`
+    ${SIZE_STYLES[size]}
+    border: 1px solid ${theme.color.neutral.border};
+    background: ${imgUrl && `url(${imgUrl})`} lightgray 50% / cover no-repeat;
+  `;
+};

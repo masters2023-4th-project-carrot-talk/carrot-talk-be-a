7aa6f43959f00861f2036e52c2419c6b2622192c
JH <vvaa277@gmail.com>
2023-08-28 16:55:32 +0900

    feat: modaltestpage업데이트

--- fe/src/pages/ModalTestPage.tsx
+++ fe/src/pages/ModalTestPage.tsx
@@ -35,8 +35,7 @@ export const ModalTestPage: FC = () => {
         <AlertButtons buttonText="취소" />
         {/* onDelete를 내려주지 않으면 buttonText로 이루어진 하나의 버튼만 렌더링 됩니다 (삭제 등 추가 기능이 필요하지 않은경우 onDelete 없이 사용하면 됩니다) */}
       </Alert>
-      {/* modal + alert가 필요한 곳에서 alert를 같이 사용해주세요 */}
-      {/* 닫는 기능은 모달과 alert내부에서 이루어집니다 사용처는 열어주는 로직과 alert에 대한 상태만 필요합니다 */}
+      {/* 닫는 기능은 모달과 alert내부에서 이루어집니다*/}
     </div>
   );
 };

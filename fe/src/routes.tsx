99447d7c482d860c18bb13a7ed0a7e481cc8d1d2
JH <vvaa277@gmail.com>
2023-08-29 01:24:08 +0900

    comment: route에 todo추가

--- fe/src/routes.tsx
+++ fe/src/routes.tsx
@@ -9,23 +9,25 @@ import { Chat } from './pages/Chat';
 import { Auth } from './pages/Auth';
 import { PATH } from './constants/path';
 
-// TODO 경로 상수화
+// TODO Private routes 구현
 export const AppRoutes: React.FC = () => {
   return (
     <div css={globalStyle}>
       <Routes>
         {/* TODO: 하단바 X - 카테고리 페이지 */}
         {/* TODO: 하단바 X - 회원가입 페이지(리다이렉트) */}
-        {/* TODO: 하단바 O - 등록페이지 */}
-        {/* TODO: 하단바 O - 상세페이지 */}
-        {/* TODO: 하단바 O - 채팅페이지 */}
+        {/* TODO: 하단바 O - 등록페이지 / 인증 필요 */}
+        {/* TODO: 하단바 O - 상세페이지 / 인증 필요*/}
+        {/* TODO: 하단바 O - 채팅페이지 / 인증 필요 */}
         <Route element={<Layout />}>
           <Route path={PATH.notFound} element={<NotFound />} />
-          {/* TODO Private routes 구현 */}
+
           <Route>
             <Route path={PATH.home} element={<Home />} />
             <Route path={PATH.sales} element={<Sales />} />
+            {/* 인증필요 */}
             <Route path={PATH.interests} element={<Interests />} />
+            {/* 인증필요 */}
             <Route path={PATH.chat} element={<Chat />} />
             <Route path={PATH.auth} element={<Auth />} />
           </Route>

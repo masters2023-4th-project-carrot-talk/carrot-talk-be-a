0e228cb84728d0163e553c0c1376a05c3167f324
JH <vvaa277@gmail.com>
2023-08-28 12:08:26 +0900

    chore: 리액트 쿼리 설치

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,5 +1,25 @@
+import { Global, ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import designSystem from './styles/designSystem';
+import { globalStyle } from './styles/globalStyle';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={designSystem}>
+        <Global styles={globalStyle} />
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;

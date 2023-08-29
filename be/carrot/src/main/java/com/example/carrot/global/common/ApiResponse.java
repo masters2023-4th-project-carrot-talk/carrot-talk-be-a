9580d9a73a0261c77132b521a53441e98e0c9730
hyeseon <chunghye1998@gmail.com>
2023-08-29 11:11:59 +0900

    feat #01: ApiResponse 생성

---
+++ be/carrot/src/main/java/com/example/carrot/global/common/ApiResponse.java
@@ -0,0 +1,41 @@
+package com.example.carrot.global.common;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+
+	private boolean success;
+	private T data;
+	private T errorCode;
+
+	@Builder
+	public ApiResponse(boolean success, T data, T errorCode) {
+		this.success = success;
+		this.data = data;
+		this.errorCode = errorCode;
+	}
+
+	public static <T> ApiResponse<T> success(boolean success) {
+		return (ApiResponse<T>)ApiResponse.builder()
+			.success(success)
+			.build();
+	}
+
+	public static <T> ApiResponse<T> success(boolean success, T data) {
+		return (ApiResponse<T>) ApiResponse.builder()
+			.success(success)
+			.data(data)
+			.build();
+	}
+
+	public static <T> ApiResponse<T> fail(boolean success, T errorCode) {
+		return (ApiResponse<T>) ApiResponse.builder()
+			.success(success)
+			.errorCode(errorCode)
+			.build();
+	}
+
+}

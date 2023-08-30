32ab847e21abe6149d8627d17e3cb923b8360bf2
hyeseon <chunghye1998@gmail.com>
2023-08-30 17:48:43 +0900

    feat #03: 로그인 필터링 구현

--- be/carrot/src/main/java/com/example/carrot/global/exception/StatusCode.java
+++ be/carrot/src/main/java/com/example/carrot/global/exception/StatusCode.java
@@ -2,12 +2,28 @@ package com.example.carrot.global.exception;
 
 import org.springframework.http.HttpStatus;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.Getter;
 
 @Getter
 public enum StatusCode {
+
+	// -- [JWT] -- //
+	NOT_FOUND_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "해당하는 리프레시 토큰을 찾을 수 없습니다."),
+	MALFORMED_JWT_EXCEPTION(HttpStatus.UNAUTHORIZED, "잘못된 형식의 토큰입니다."),
+	EXPIRED_JWT_EXCEPTION(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+	SIGNATURE_EXCEPTION(HttpStatus.UNAUTHORIZED, "JWT의 서명이 올바르지 않습니다."),
+	UNSUPPORTED_JWT_EXCEPTION(HttpStatus.UNAUTHORIZED, "지원하지 않는 토큰입니다."),
+	ILLEGAL_ARGUMENT_EXCEPTION(HttpStatus.UNAUTHORIZED, "잘못된 인자입니다."),
+
 	// -- [USER] -- //
-	ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "같은 아이디가 존재합니다.");
+	ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "같은 아이디가 존재합니다."),
+
+	// -- [LOCATION] -- //
+	NOT_FOUND_LOCATION(HttpStatus.BAD_REQUEST, "존재하지 않는 Location입니다.");
 
 	private HttpStatus status;
 	private String message;
@@ -15,5 +31,21 @@ public enum StatusCode {
 	StatusCode(HttpStatus status, String message) {
 		this.status = status;
 		this.message = message;
+	}
+
+	public static StatusCode from(RuntimeException e) {
+		if (e instanceof MalformedJwtException) {
+			return StatusCode.MALFORMED_JWT_EXCEPTION;
+		}
+		if (e instanceof ExpiredJwtException) {
+			return StatusCode.EXPIRED_JWT_EXCEPTION;
+		}
+		if (e instanceof SignatureException) {
+			return StatusCode.SIGNATURE_EXCEPTION;
+		}
+		if (e instanceof UnsupportedJwtException) {
+			return StatusCode.UNSUPPORTED_JWT_EXCEPTION;
+		}
+		return StatusCode.ILLEGAL_ARGUMENT_EXCEPTION;
 	}
 }
---
+++ be/carrot/src/main/java/com/example/carrot/global/filter/AuthFilter.java
@@ -0,0 +1,104 @@
+package com.example.carrot.global.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.util.PatternMatchUtils;
+
+import com.example.carrot.global.common.ApiResponse;
+import com.example.carrot.global.exception.ErrorCode;
+import com.example.carrot.global.exception.StatusCode;
+import com.example.carrot.global.jwt.JwtProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.MalformedJwtException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AuthFilter implements Filter {
+
+	private final String[] whiteListUris = new String[] {"/", "/api/users/login", "/api/users/signup", "/oauth/redirect"};
+
+	private final ObjectMapper objectMapper;
+
+	private final JwtProvider jwtProvider;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		log.info("doFilter 진입");
+		HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+		HttpServletResponse httpServletResponse = (HttpServletResponse)response;
+
+		if (whiteListCheck(httpServletRequest.getRequestURI())) {
+			log.info("whileListCheck 진입");
+			log.info("request uri : {}", httpServletRequest.getRequestURI());
+			if (httpServletRequest.getRequestURI().equals("/api/users/signup")) {
+				Claims claims = jwtProvider.getClaims(getToken(httpServletRequest));
+				request.setAttribute("socialId", claims.get("socialId"));
+				request.setAttribute("imgUrl", claims.get("imgUrl"));
+			}
+			chain.doFilter(request, response);
+			return;
+		}
+
+		if (!isContainToken(httpServletRequest)) {
+			log.info("isContainToken 진입");
+			sendErrorApiResponse(httpServletResponse, new MalformedJwtException(""));
+			return;
+		}
+
+		try {
+			log.info("로그인 인증");
+			String token = getToken(httpServletRequest);
+			Claims claims = jwtProvider.getClaims(token);
+			request.setAttribute("userId", claims.get("userId"));
+			chain.doFilter(request, response);
+		} catch (RuntimeException e) {
+			log.debug(e.getClass().getName());
+			sendErrorApiResponse(httpServletResponse, e);
+		}
+	}
+
+	private void sendErrorApiResponse(HttpServletResponse response, RuntimeException e) throws IOException {
+		response.setCharacterEncoding("UTF-8");
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+		response.getWriter().write(
+			objectMapper.writeValueAsString(generateErrorApiResponse(e)));
+	}
+
+	private ApiResponse<ErrorCode> generateErrorApiResponse(RuntimeException e) {
+		StatusCode statusCode = StatusCode.from(e);
+
+		return ApiResponse.fail(new ErrorCode(statusCode.getStatus(), statusCode.getMessage()));
+	}
+
+	private boolean isContainToken(HttpServletRequest httpServletRequest) {
+		String authorization = httpServletRequest.getHeader("Authorization");
+		return authorization != null && authorization.startsWith("Bearer ");
+	}
+
+	private boolean whiteListCheck(String uri) {
+		return PatternMatchUtils.simpleMatch(whiteListUris, uri);
+	}
+
+	private String getToken(HttpServletRequest request) {
+		String authorization = request.getHeader("Authorization");
+		return authorization.substring(7);
+
+	}
+}

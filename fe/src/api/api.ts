22e3314a5cdfe4334bee8cf0790a893823003d58
JH <vvaa277@gmail.com>
2023-08-31 17:57:23 +0900

    chore: api 파일 추가

---
+++ fe/src/api/api.ts
@@ -0,0 +1,47 @@
+export const BASE_URL = 'http://localhost:5173';
+
+const fetchData = async (path: string, options?: RequestInit) => {
+  const response = await fetch(BASE_URL + path, options);
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  if (response.headers.get('content-type') === 'application/json') {
+    const data = response.json();
+
+    return data;
+  }
+
+  throw new Error('Content type is not json');
+};
+
+export const getMyLocations = async () => {
+  // TODO 액세스 토큰을 헤더에 담아서 보내야 함
+  // TODO const accesToken = null;
+
+  // TODO  if (!accesToken) return {id: 0, name: '역삼 1동', isMainLocation: true};
+
+  return await fetchData('/users/locations');
+};
+
+export const deleteLocation = async (id: number) => {
+  // TODO 액세스 토큰을 헤더에 담아서 보내야 함
+  // TODO const accesToken =
+
+  return await fetchData(`/users/locations/${id}`, {
+    method: 'DELETE',
+  });
+};
+
+export const patchMainLocation = async (id: number) => {
+  // TODO 액세스 토큰을 헤더에 담아서 보내야 함
+  // TODO const accesToken =
+
+  return await fetchData(`/users/locations`, {
+    method: 'PATCH',
+    body: JSON.stringify({ id }),
+  });
+};
+
+// export const postLocation = async (name: string) => {

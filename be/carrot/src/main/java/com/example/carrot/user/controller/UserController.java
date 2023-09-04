d9c3d4f722147373d5e4077dd832213e8daca246
Sully <2dcoder@naver.com>
2023-09-04 17:16:15 +0900

    feat #23: 동네 삭제를 위한 Response Dto, Controller 구현

--- be/carrot/src/main/java/com/example/carrot/user/controller/UserController.java
+++ be/carrot/src/main/java/com/example/carrot/user/controller/UserController.java
@@ -2,8 +2,11 @@ package com.example.carrot.user.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +17,7 @@ import com.example.carrot.user.dto.request.LogoutRequestDto;
 import com.example.carrot.user.dto.request.ReissueRequestDto;
 import com.example.carrot.user.dto.request.SignUpRequestDto;
 import com.example.carrot.user.dto.response.ReissueResponseDto;
+import com.example.carrot.user.dto.response.UserLocationDeleteResponseDto;
 import com.example.carrot.user.dto.response.UserResponseDto;
 import com.example.carrot.user.service.UserService;
 
@@ -78,5 +82,14 @@ public class UserController {
 		userService.kakaoLogout(logoutRequestDto, userId);
 
 		return ApiResponse.success();
+	}
+
+	/**
+	 * 동네 삭제 API
+	 */
+	@DeleteMapping("/users/locations/{locationId}")
+	public ApiResponse<UserLocationDeleteResponseDto> deleteUserLocation(@PathVariable Long locationId,
+		@RequestAttribute Long userId) {
+		return ApiResponse.success(userService.deleteUserLocation(locationId, userId));
 	}
 }

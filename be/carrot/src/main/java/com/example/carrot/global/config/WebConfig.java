9a249d93121a0a9006d625bfa7cd9fdca5d33524
hyeseon <chunghye1998@gmail.com>
2023-09-05 16:29:52 +0900

    chore #32: 불필요한 라이브러리 사용 변경 (netty 삭제)

--- be/carrot/src/main/java/com/example/carrot/global/config/WebConfig.java
+++ be/carrot/src/main/java/com/example/carrot/global/config/WebConfig.java
@@ -1,10 +1,9 @@
 package com.example.carrot.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import io.netty.handler.codec.http.HttpMethod;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {

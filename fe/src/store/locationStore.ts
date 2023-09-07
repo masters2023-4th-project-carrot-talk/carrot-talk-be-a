422717ed1c7baefa278e0868bbbe7ac9a45dfc36
JH <vvaa277@gmail.com>
2023-09-07 18:40:28 +0900

    Auto stash before merge of "auth-location-test-w3" and "origin/auth-location-test-w3"

--- fe/src/store/locationStore.ts
+++ fe/src/store/locationStore.ts
@@ -9,3 +9,16 @@ export const useLocationStore = create<LocationState>((set) => ({
   isMainLocationSet: false,
   setIsMainLocationSet: () => set({ isMainLocationSet: true }),
 }));
+
+type RegisteredLocationsState = {
+  locationList: LocationType[];
+  setAddLocation: (location: LocationType) => void;
+};
+
+export const useRegisteredLocationsStore = create<RegisteredLocationsState>(
+  (set) => ({
+    locationList: [],
+    setAddLocation: (location: LocationType) =>
+      set((state) => ({ locationList: [...state.locationList, location] })),
+  }),
+);

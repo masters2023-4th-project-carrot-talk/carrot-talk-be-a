a26f1e4c1331f9abf082fc74bbcc9f564090caab
hyeseon <chunghye1998@gmail.com>
2023-09-08 01:45:32 +0900

    fix #41: StatusCode에서 fallback 용으로 사용할 unknown exception 생성

--- be/carrot/src/main/java/com/example/carrot/global/exception/StatusCode.java
+++ be/carrot/src/main/java/com/example/carrot/global/exception/StatusCode.java
@@ -18,6 +18,7 @@ public enum StatusCode {
 	SIGNATURE_EXCEPTION(HttpStatus.UNAUTHORIZED, "JWT의 서명이 올바르지 않습니다."),
 	UNSUPPORTED_JWT_EXCEPTION(HttpStatus.UNAUTHORIZED, "지원하지 않는 토큰입니다."),
 	ILLEGAL_ARGUMENT_EXCEPTION(HttpStatus.UNAUTHORIZED, "잘못된 인자입니다."),
+	UNKNOWN_EXCEPTION(HttpStatus.UNAUTHORIZED, "알 수 없는 오류가 발생했습니다." ),
 
 	// -- [USER] -- //
 	ALREADY_EXIST_USER(HttpStatus.CONFLICT, "같은 닉네임이 존재합니다."),
@@ -62,6 +63,9 @@ public enum StatusCode {
 		if (e instanceof UnsupportedJwtException) {
 			return StatusCode.UNSUPPORTED_JWT_EXCEPTION;
 		}
-		return StatusCode.ILLEGAL_ARGUMENT_EXCEPTION;
+		if (e instanceof IllegalArgumentException) {
+			return StatusCode.ILLEGAL_ARGUMENT_EXCEPTION;
+		}
+		return StatusCode.UNKNOWN_EXCEPTION;
 	}
 }

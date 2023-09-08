f3f40d1b581abd368a1d39d9bddb2dfbdefed3ad
hyeseon <chunghye1998@gmail.com>
2023-09-08 11:03:18 +0900

    fix #41: ProductStatus 기본적으로 on_sale 반환하도록 변경

--- be/carrot/src/main/java/com/example/carrot/product/entity/ProductStatus.java
+++ be/carrot/src/main/java/com/example/carrot/product/entity/ProductStatus.java
@@ -3,17 +3,16 @@ package com.example.carrot.product.entity;
 public enum ProductStatus {
 	ON_SALE, SOLD_OUT, RESERVED;
 
-	// 단순 choose 로직이라 예외 처리는 그 전 단계에서 해야 함
 	public static ProductStatus chooseStatus(final String statusString) {
-		if (statusString.equalsIgnoreCase("ON_SALE")) {
-			return ProductStatus.ON_SALE;
+		if (statusString.equalsIgnoreCase("RESERVED")) {
+			return ProductStatus.RESERVED;
 		}
 
 		if (statusString.equalsIgnoreCase("SOLD_OUT")) {
 			return ProductStatus.SOLD_OUT;
 		}
 
-		return ProductStatus.RESERVED;
+		return ProductStatus.ON_SALE;
 	}
 
 	public String getName() {

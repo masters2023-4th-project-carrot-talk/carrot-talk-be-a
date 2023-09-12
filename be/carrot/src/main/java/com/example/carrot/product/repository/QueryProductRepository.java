739ad437fa04c832c4ad7706feb7d262f0ca37c5
hyeseon <chunghye1998@gmail.com>
2023-09-12 17:22:35 +0900

    refactor #43: ProductRepositoryCustom -> QueryProductRepository 로 네이밍 변경

--- be/carrot/src/main/java/com/example/carrot/product/repository/QueryProductRepository.java
+++ be/carrot/src/main/java/com/example/carrot/product/repository/QueryProductRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.domain.Slice;
 
 import com.example.carrot.product.dto.response.ProductsResponseDto;
 
-public interface ProductRepositoryCustom {
+public interface QueryProductRepository {
 	Slice<ProductsResponseDto> findProducts(Long locationId, Long categoryId, Long next, int size);
 }

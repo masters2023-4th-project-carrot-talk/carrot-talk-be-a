c73ab34deb71a6f275e571f327349a8dab6e331e
jello <song6240@naver.com>
2023-09-19 00:44:25 +0900

    refactor: 백드롭 레이아웃 수정

--- fe/src/components/common/dropdown/Dropdown.tsx
+++ fe/src/components/common/dropdown/Dropdown.tsx
@@ -2,6 +2,7 @@ import { ButtonProps } from '@components/common/button/Button';
 import { css } from '@emotion/react';
 import { cloneElement, useState } from 'react';
 import { MenuBoxProps } from '../menu/MenuBox';
+import { Backdrop } from './Backdrop';
 
 type Props = {
   opener: React.ReactElement<ButtonProps>;
@@ -37,11 +38,7 @@ export const Dropdown: React.FC<Props> = ({ opener, menu, align }) => {
       </div>
       {isOpen && (
         <>
-          {/* <Backdrop
-            onClick={(e) => {
-              closeMenu(e); // BUG dropdown 버그로 인해 임시 추가
-            }}
-          /> */}
+          <Backdrop onClick={closeMenu} />
           {cloneElement(menu, { onClick: closeMenu })}
         </>
       )}

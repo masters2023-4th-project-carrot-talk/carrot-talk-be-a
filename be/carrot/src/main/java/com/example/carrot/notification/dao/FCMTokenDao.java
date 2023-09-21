02a0fe709487c727f4e6189c035137077a90a510
Sully <2dcoder@naver.com>
2023-09-21 22:14:07 +0900

    feat #110: FCM Token 관련 코드 구현
    - 토큰은 UUID로 서버에서 임의로 지정

---
+++ be/carrot/src/main/java/com/example/carrot/notification/dao/FCMTokenDao.java
@@ -0,0 +1,39 @@
+package com.example.carrot.notification.dao;
+
+import java.util.UUID;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.carrot.global.exception.CustomException;
+import com.example.carrot.global.exception.StatusCode;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Repository
+public class FCMTokenDao {
+
+	private final StringRedisTemplate tokenRedisTemplate;
+
+	public void saveToken(String userId) {
+		if (!hasKey(userId)) {
+			throw new CustomException(StatusCode.ALREADY_EXIST_USER);
+		}
+
+		tokenRedisTemplate.opsForValue()
+			.set(userId, UUID.randomUUID().toString());
+	}
+
+	public String getToken(String userId) {
+		return tokenRedisTemplate.opsForValue().get(userId);
+	}
+
+	public void deleteToken(String userId) {
+		tokenRedisTemplate.delete(userId);
+	}
+
+	public boolean hasKey(String userId) {
+		return Boolean.TRUE.equals(tokenRedisTemplate.hasKey(userId));
+	}
+}

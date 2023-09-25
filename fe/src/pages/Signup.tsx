fd5ab299e3134abf4d3418a391c2b98543961e21
jello <song6240@naver.com>
2023-09-25 11:00:26 +0900

    fix: 동네 2개 등록을 회원가입 조건으로 변경

--- fe/src/pages/Signup.tsx
+++ fe/src/pages/Signup.tsx
@@ -38,28 +38,28 @@ export const Signup: React.FC = () => {
   const signupMutation = useSignup();
 
   const submitEnabled =
-    isUniqueNickname && localLocations && localLocations.length > 0;
+    isUniqueNickname && localLocations && localLocations.length === 2;
 
   const goToAuth = () => {
     navigate(PATH.account, { replace: true });
   };
 
   const requestSignup = () => {
-    if (!submitEnabled) {
+    const mainLocation = localLocations.find(
+      (location: LocationType) => location.isMainLocation,
+    );
+    const subLocation = localLocations.find(
+      (location: LocationType) => !location.isMainLocation,
+    );
+
+    if (!submitEnabled || !mainLocation || !subLocation) {
       return;
     }
 
-    const mainLocationId = localLocations.find(
-      (location: LocationType) => location.isMainLocation,
-    )?.id as number;
-    const subLocationId = localLocations?.find(
-      (location: LocationType) => !location.isMainLocation,
-    )?.id;
-
     const signupInfo = {
       nickname,
-      mainLocationId,
-      ...(!!subLocationId && { subLocationId }),
+      mainLocationId: mainLocation.id,
+      subLocationId: subLocation.id,
     };
 
     signupMutation.mutate(signupInfo, {

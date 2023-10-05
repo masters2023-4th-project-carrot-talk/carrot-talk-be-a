191ef57bf1c1f8c8b3931e31104b6c7ef197aea9
JH <vvaa277@gmail.com>
2023-10-05 12:34:21 +0900

    chore: 채팅 query 안쓰는 코드 삭제

--- fe/src/queries/chat.ts
+++ fe/src/queries/chat.ts
@@ -17,70 +17,6 @@ export const useChatRooms = () =>
     select: (data) => data.data,
   });
 
-
-//   return { chatRooms, status, error };
-// };
-
-export const useUnreadTotalCount = () => {
-  // const { chatRooms } = useChatRooms();
-
-  // const unreadTotalCount = chatRooms?.reduce((acc, cur) => {
-  //   return acc + cur.unreadChatCount;
-  // }, 0); // 메모?
-
-  // return { unreadTotalCount };
-
-  const { data: unreadTotalCount } = useQuery<
-    UnreadTotalFromServer,
-    unknown,
-    number,
-    unknown
-  >({
-    mutationFn: (productId: number) => getChatRoomId(productId),
-  });
-
-// export const useChatRooms = () => {
-//   const {
-//     data: chatRooms,
-//     status,
-//     error,
-//   } = useQuery<ChatRoomsDataFromServer, unknown, ChatRoomType[]>(
-//     QUERY_KEY.chatRooms,
-//     getChatRooms,
-//     {
-//       select: (data) => data.data,
-//     },
-//   );
-
-//   return { chatRooms, status, error };
-// };
-
-export const useChatRooms = () =>
-  useQuery({
-    queryKey: QUERY_KEY.chatRooms,
-    queryFn: () => getChatRooms(),
-    select: (data) => data.data,
-  });
-
-// export const useUnreadTotalCount = () => {
-//   // const { chatRooms } = useChatRooms();
-
-//   // const unreadTotalCount = chatRooms?.reduce((acc, cur) => {
-//   //   return acc + cur.unreadChatCount;
-//   // }, 0); // 메모?
-
-//   // return { unreadTotalCount };
-
-//   const { data: unreadTotalCount } = useQuery<
-//     UnreadTotalFromServer,
-//     unknown,
-//     UnreadTotalCountType
-//   >(QUERY_KEY.unreadTotalCount, getUnreadTotalCount, {
-//     select: (data) => data.data,
-//   });
-//   return { unreadTotalCount };
-// };
-
 export const useUnreadTotalCount = (isLogin: boolean) =>
   useQuery({
     queryKey: QUERY_KEY.unreadTotalCount,
@@ -129,19 +65,6 @@ export const useChatRoomInfo = (chatroomId: number) => {
   };
 };
 
-
-import {
-  getChatRoomHistories,
-  getChatRoomId,
-  getChatRoomInfo,
-  getChatRooms,
-  getUnreadTotalCount,
-} from '@api/api';
-import { QUERY_KEY } from '@constants/queryKey';
-import { createQueryParams } from '@utils/createQueryParams';
-import { useCallback, useMemo } from 'react';
-import { useInfiniteQuery, useMutation, useQuery } from 'react-query';
-
 export const useChatRoomId = () =>
   useMutation<
     ResponseFromServer<{ chatroomId: number }>,
@@ -151,77 +74,3 @@ export const useChatRoomId = () =>
   >({
     mutationFn: (productId: number) => getChatRoomId(productId),
   });
-
-export const useChatRooms = () => {
-  const {
-    data: chatRooms,
-    status,
-    error,
-  } = useQuery<ChatRoomsDataFromServer, unknown, ChatRoomType[]>(
-    QUERY_KEY.chatRooms,
-    getChatRooms,
-    {
-      select: (data) => data.data,
-    },
-  );
-
-  return { chatRooms, status, error };
-};
-
-export const useUnreadTotalCount = () => {
-  // const { chatRooms } = useChatRooms();
-
-  // const unreadTotalCount = chatRooms?.reduce((acc, cur) => {
-  //   return acc + cur.unreadChatCount;
-  // }, 0); // 메모?
-
-  // return { unreadTotalCount };
-
-  const { data: unreadTotalCount } = useQuery<
-    UnreadTotalFromServer,
-    unknown,
-    UnreadTotalCountType
-  >(QUERY_KEY.unreadTotalCount, getUnreadTotalCount, {
-    select: (data) => data.data,
-  });
-  return { unreadTotalCount };
-};
-
-export const useChatRoomHistories = (chatroomId: number) => {
-  const fetchHistories = useCallback(
-    ({ pageParam }: { pageParam?: number | string }) => {
-      const queryParam = createQueryParams({ next: pageParam });
-      return getChatRoomHistories(chatroomId, queryParam);
-    },
-    [chatroomId],
-  );
-
-  const query = useInfiniteQuery({
-    queryKey: ['chatRoomHistories', chatroomId],
-    queryFn: fetchHistories,
-    getNextPageParam: (lastPage) => lastPage.data?.nextId,
-  });
-
-  const allHistories = useMemo(
-    () =>
-      query.data?.pages.flatMap((page) => page.data.chattings).reverse() ?? [],
-    [query.data?.pages],
-  );
-
-  return {
-    ...query,
-    data: allHistories,
-  };
-};
-
-export const useChatRoomInfo = (chatroomId: number) => {
-  const query = useQuery<ResponseFromServer<ChatRoomInfo>, unknown>({
-    queryKey: ['chatRoomInfo', chatroomId],
-    queryFn: () => getChatRoomInfo(chatroomId),
-  });
-
-  return {
-    ...query,
-    data: query.data?.data,
-  };
-};
